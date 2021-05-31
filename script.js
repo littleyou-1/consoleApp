affae43c59ab6f4142f04eb12a98d2cc53df2c42
Ivan <ivanovyz25@gmail.com>
2021-05-31 19:34:11 +0300

    After second lesson

--- script.js
+++ script.js
@@ -1,46 +1,17 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
 'use strict';
 
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "2");
+if (4 == 9) {
+    console.log("ok!");
+} else {
+    console.log("Error");
+}
 
-const personalMovieDB = {
+const num = 50;
 
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-const nameOfFilms = prompt("Один из последних просмотренных фильмов?", "");
-const starOfFilms = prompt("На сколько оцените его?", "");
-
-const nameOfFilms1 = prompt("Один из последних просмотренных фильмов?", "");
-const starOfFilms1 = prompt("На сколько оцените его?", "");
-
-personalMovieDB.movies[nameOfFilms] = starOfFilms;
-personalMovieDB.movies[nameOfFilms1] = starOfFilms1;
-
-console.log(personalMovieDB);
+if (num < 49 ) {
+    console.log("Error");
+} else if (num > 100 ) {
+    console.log("mnogo");
+} else{
+    console.log("graet");
+}
