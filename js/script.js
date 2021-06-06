ce415925f2543e16517abd1bb6384dccb7021857
Ivan <ivanovyz25@gmail.com>
2021-06-06 16:16:10 +0300

    After  lesson about work with pages data

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
 'use strict';
 
+
+const box = document.getElementById('box');
+
+console.log(box);
+
+const buttons = document.getElementsByTagName('button');
+
+console.log(buttons[1]);
+
+const cirkles = document.getElementsByClassName('circle');
+console.log(cirkles);
+
+const hearts = document.querySelectorAll('.heart');
+
+hearts.forEach(item => {
+    console.log(item);
+});
+
+const oheHeart = document.querySelector('.heart');
+
+console.log(oheHeart);
