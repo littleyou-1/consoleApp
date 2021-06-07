daee3a4d8d9a90f10aa7d13e627ad826f47fade6
Ivan <ivanovyz25@gmail.com>
2021-06-07 13:19:13 +0300

    some actions with page elevents

--- js/script.js
+++ js/script.js
@@ -1,23 +1,50 @@
 'use strict';
 
 
-const box = document.getElementById('box');
+const box = document.getElementById('box'),
+    buttons = document.getElementsByTagName('button'),
+    cirkles = document.getElementsByClassName('circle'),
+    hearts = document.querySelectorAll('.heart'),
+    oheHeart = document.querySelector('.heart'),
+    wrapper = document.querySelector('.wrapper');
 
-console.log(box);
 
-const buttons = document.getElementsByTagName('button');
+/* box.style.backgroundColor = 'blue';
+box.style.width = '500px'; */
 
-console.log(buttons[1]);
+box.style.cssText = `background-clolr: blue; width: 500px`;
 
-const cirkles = document.getElementsByClassName('circle');
-console.log(cirkles);
+buttons[1].style.borderRadius = '50%';
+cirkles[0].style.backgroundColor = 'red';
 
-const hearts = document.querySelectorAll('.heart');
+/* for  (let i = 0; i < hearts.length;  i++) {
+    hearts[i].style.backgroundColor = 'blue';
+} */
 
 hearts.forEach(item => {
-    console.log(item);
+    item.style.backgroundColor = 'blue';
 });
 
-const oheHeart = document.querySelector('.heart');
+const div = document.createElement('div');
 
-console.log(oheHeart);
+///const text = document.createTextNode('Hear am i');
+
+div.classList.add('black');
+
+
+document.body.append(div);
+
+//document.append(div);
+
+//wrapper.prepend(div);
+
+//hearts[1].after(div);
+
+//cirkles[1].remove();
+
+//hearts[0].replaceWith(cirkles[0]);
+
+div.innerHTML = "<h1>Hello World</h1>";
+
+div.insertAdjacentHTML("", '<h2>Hello</h2>');
+ 
